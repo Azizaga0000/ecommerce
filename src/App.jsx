b31837a6ef34ffdbc080c1302ecc3e793dbd2aea
Azizaga0000 <dev.bash.wrld@outlook.com>
2026-02-04 22:20:50 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import AppRoutes from './components/AppRoutes'
+import { AuthProvider } from './components/AuthContext'
 
 function App() {
+
   return (
     <>
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </>
   )
 }
